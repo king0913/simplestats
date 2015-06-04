4efc9651ef7704901245daac034652dfa4ede844
king0913 <king0913@users.noreply.github.com>
2015-06-04 16:03:41 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -11,7 +11,7 @@ def mean(vals):
 def median(vals):
     vals.sort()
     z = len(vals)
-    index = z / 2
+    list = z / 2
     if z % 2 == 0:
        return mean([vals[index], vals[index - 1]])
     else:
